0ec8e94ba376a64bd19111008587efccb56250e1
SmolNero <smolnero@gmail.com>
2025-01-07 18:54:42 -0700

    bonethug

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 // villin_REIGN - Military-grade IoT security
 
-pub mod core;;
+pub mod core;
+"""
+    - Declares and makes the core module public
+    - Looks for either src/core/mod.rs or src/core.rs
+    - 'pub' meanss ither code can access this module
+"""
 pub mod security;
+"""
+    - Declares and makes the security public
+    - Look for src/security/mod.rs or src/security.rs
+"""
 pub mod api;
 pub mod monitoring;
 
