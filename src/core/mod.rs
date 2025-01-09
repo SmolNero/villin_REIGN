2a5da1cd30448f22f782234e1a66d169fd43ca52
SmolNero <smolnero@gmail.com>
2025-01-08 18:10:21 -0700

    olo

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -20,7 +20,15 @@ impl REIGN {
 		// Adding methdods to REIGN struct
 		// All functions (methods) -> to REIGN stuct above		
 	pub fn new(config: Config) -> Result<Self, Box<dyn::error::Error>> {
+		// Makes public function, to call -> REIGN::new()
+	  	// fn new(config: Config) -> defines a func named 'new', takes one argument
+		// config: Config -> parameter named 'config' of Type Config
+		// -> Result<> specifies return type
+		//	   - Result is an enum for success/failure handling		
+		//	   - Self refers to the REIGN type
+		//	   - Box<dyn std::error::Error> is a trait object for error handling
 		Ok(Self {
+			// Ok is a success variant of Result
 			config,
 			state: State::default(),
 		})
