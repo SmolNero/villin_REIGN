fb401be2b5f301b7b8969c79e0cb8013ae98b013
SmolNero <smolnero@gmail.com>
2025-01-09 17:44:43 -0700

    config.rs

---
+++ src/core/config.rs
@@ -0,0 +1,12 @@
+// src/core/config.rs
+
+pub struct Config {
+	// Configuration fields will go here
+	pub api_key: String,
+	pub Port: u16,
+}
+
+impl Config {
+	pub fn new(api_key: Stringm port:)	
+}
+
